465bebfba903fe8f0e1fa9cbfa8d144569376f66
Dew <elkokmedo@gmail.com>
2024-09-25 15:25:39 +0300

    Merge branch 'refs/heads/edit_profile'

--- lib/features/database/presentation/views/widgets/database_list.dart
+++ lib/features/database/presentation/views/widgets/database_list.dart
@@ -30,12 +30,12 @@ class DataBaseList extends StatelessWidget {
           top: 10.0,
           left: 15.0,
           right: 15.0,
-        ),
+        ).r,
         child: Column(
           children: [
             // Search and Date Picker always visible
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              padding: const EdgeInsets.symmetric(vertical: 10.0).r,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -47,58 +47,54 @@ class DataBaseList extends StatelessWidget {
                         label: 'Search',
                         suffix: AppIcons.search,
                         contentPadding: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 20),
+                             horizontal: 20).r,
                         onChange: (value) {
                           controller.searchTasks(value);
                         },
                       ),
                     ),
                   ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: AppColors.current.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Obx(() => Text(
-                                  '${controller.selectedDate.value.year}/${controller.selectedDate.value.month}/${controller.selectedDate.value.day}',
-                                  style: Styles.textStyleBold
-                                      .copyWith(fontSize: 14),
-                                )),
-                            const Spacer(),
-                            IconButton(
-                              onPressed: () async {
-                                DateTime? newDate = await showDatePicker(
-                                  context: context,
-                                  firstDate: DateTime(2024),
-                                  lastDate: DateTime(2100),
-                                  initialDate: controller.selectedDate.value,
-                                );
-                                if (newDate != null) {
-                                  controller.updateSelectedDate(newDate);
-                                }
-                              },
-                              icon: AppIcons.calendar,
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                controller
-                                    .resetDate(); // Reset to show all tasks
-                              },
-                              icon: Icon(Icons.close,
-                                  color: AppColors.current.text),
-                            ),
-                          ],
-                        ),
+                  5.horizontalSpace,
+                  Container(
+                    height: 43.r,
+                    decoration: BoxDecoration(
+                      color: AppColors.current.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0).r,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Obx(() => Text(
+                                '${controller.selectedDate.value.year}/${controller.selectedDate.value.month}/${controller.selectedDate.value.day}',
+                                style: Styles.textStyleBold
+                                    .copyWith(fontSize: 14.spMin),
+                              )),
+                          IconButton(
+                            onPressed: () async {
+                              DateTime? newDate = await showDatePicker(
+                                context: context,
+                                firstDate: DateTime(2024),
+                                lastDate: DateTime(2100),
+                                initialDate: controller.selectedDate.value,
+                              );
+                              if (newDate != null) {
+                                controller.updateSelectedDate(newDate);
+                              }
+                            },
+                            icon: AppIcons.calendar,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              controller
+                                  .resetDate(); // Reset to show all tasks
+                            },
+                            icon: Icon(Icons.close,
+                                color: AppColors.current.text),
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -121,10 +117,10 @@ class DataBaseList extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         const Image(image: AssetImage(Assets.noData)),
-                        const SizedBox(height: 20),
+                        20.verticalSpace,
                         Text(
                           'No tasks found!',
-                          style: Styles.textStyleBold.copyWith(fontSize: 18),
+                          style: Styles.textStyleBold.copyWith(fontSize: 18.spMin),
                         ),
                       ],
                     ),
@@ -135,7 +131,7 @@ class DataBaseList extends StatelessWidget {
                 return ListView.separated(
                   itemCount: controller.filteredTasks.length,
                   separatorBuilder: (context, index) =>
-                      const SizedBox(height: 15),
+                      15.verticalSpace,
                   itemBuilder: (context, index) {
                     final task = controller.filteredTasks[index];
                     return Container(
@@ -149,44 +145,44 @@ class DataBaseList extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(
                           horizontal: 20.0,
                           vertical: 10.0,
-                        ),
+                        ).r,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
                               'الاسم : ${task['name']}',
                               style:
-                                  Styles.textStyleBold.copyWith(fontSize: 16),
+                                  Styles.textStyleBold.copyWith(fontSize: 16.spMin),
                             ),
-                            const SizedBox(height: 5),
+                            5.verticalSpace,
                             Text(
                               'الهاتف : ${task['phone']}',
                               style:
-                                  Styles.textStyleBold.copyWith(fontSize: 16),
+                                  Styles.textStyleBold.copyWith(fontSize: 16.r),
                             ),
-                            const SizedBox(height: 5),
+                            5.verticalSpace,
                             Text(
                               'العمر : ${task['age']}',
                               style:
-                                  Styles.textStyleBold.copyWith(fontSize: 16),
+                                  Styles.textStyleBold.copyWith(fontSize: 16.spMin),
                             ),
-                            const SizedBox(height: 5),
+                            5.verticalSpace,
                             Text(
                               'المنطقه : ${task['area']}',
                               style:
                                   Styles.textStyleBold.copyWith(fontSize: 16),
                             ),
-                            const SizedBox(height: 5),
+                           5.verticalSpace,
                             Text(
                               'الاصابه : ${task['injury']}',
                               style:
-                                  Styles.textStyleBold.copyWith(fontSize: 16),
+                                  Styles.textStyleBold.copyWith(fontSize: 16.spMin),
                             ),
                             const Spacer(),
                             Text(
                               'التاريخ : ${task['date'].toString().substring(0, 19)}',
                               style:
-                                  Styles.textStyleBold.copyWith(fontSize: 16),
+                                  Styles.textStyleBold.copyWith(fontSize: 16.spMin),
                             ),
                           ],
                         ),
