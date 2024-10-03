1933be0f9b6be5558ec103db3e9e5a701883e20f
Dew <elkokmedo@gmail.com>
2024-10-03 23:04:13 +0300

    edit some changes in home page ui

--- lib/features/desktop_layout/home/presentations/views/widgets/desktop_home_body.dart
+++ lib/features/desktop_layout/home/presentations/views/widgets/desktop_home_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tamredak/core/themes/app_colors.dart';
 import 'package:tamredak/core/utils/assets.dart';
+import 'package:tamredak/core/utils/styles.dart';
 import 'package:tamredak/features/desktop_layout/home/presentations/views/widgets/desktop_home_container.dart';
 
 class DesktopHomeBody extends StatelessWidget {
@@ -16,79 +17,92 @@ class DesktopHomeBody extends StatelessWidget {
         decoration: BoxDecoration(
             color: AppColors.current.blueBackground,
             borderRadius: BorderRadius.circular(20)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
+            const Spacer(),
+            Text(
+              'مرحبا بك في تطبيق تمريضك ',
+              style: Styles.textStyleBold.copyWith(
+                fontSize: responsiveFonts(context, fontSize: 20),
+                color: AppColors.current.white
+              ),
+            ),
+            const Spacer(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                DesktopHomeContainer(
-                  image: Image(
-                    width: 140.r,
-                    height: 140.r,
-                    fit: BoxFit.cover,
-                    image: const AssetImage(
-                      Assets.card1home,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DesktopHomeContainer(
+                      image: Image(
+                        width: 140.r,
+                        height: 140.r,
+                        fit: BoxFit.cover,
+                        image: const AssetImage(
+                          Assets.card1home,
+                        ),
+                      ),
+                      text: 'من خلال هذا التطبيق يمكنك اضافة ممرض جديد للنظام',
+                      color: AppColors.current.greenButtons,
+                      backcolor: AppColors.current.darkGreen.withOpacity(0.5),
+                      test: true,
                     ),
-                  ),
-                  text: 'اضافة ممرض جديد للنظام',
-                  color: AppColors.current.greenButtons,
-                  backcolor: AppColors.current.darkGreen.withOpacity(0.5),
-                  test: true,
-
+                    15.verticalSpace,
+                    DesktopHomeContainer(
+                      image: Image(
+                        width: 140.r,
+                        height: 140.r,
+                        fit: BoxFit.cover,
+                        image: const AssetImage(
+                          Assets.card4home,
+                        ),
+                      ),
+                      text: 'ايضا يمكنك رؤية جميع الممرضين بالنظام',
+                      color: AppColors.current.blueText,
+                      backcolor: AppColors.current.darkBlue.withOpacity(0.5),
+                      test: false,
+                    )
+                  ],
                 ),
-                15.verticalSpace,
-                DesktopHomeContainer(
-                  image: Image(
-                    width: 140.r,
-                    height: 140.r,
-                    fit: BoxFit.cover,
-                    image: const AssetImage(
-                      Assets.card4home,
+                15.horizontalSpace,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DesktopHomeContainer(
+                      image: Image(
+                        width: 140.r,
+                        height: 140.r,
+                        fit: BoxFit.cover,
+                        image: const AssetImage(
+                          Assets.card2home,
+                        ),
+                      ),
+                      text: 'ايضا يمكنك رؤية الممرضين المتاحين',
+                      color: AppColors.current.orangeButtons,
+                      backcolor: AppColors.current.darkOrange.withOpacity(0.5),
+                      test: false,
                     ),
-                  ),
-                  text: 'رؤية جميع الممرضين بالنظام',
-                  color: AppColors.current.blueText,
-                  backcolor: AppColors.current.darkBlue.withOpacity(0.5),
-                  test: false,
-                )
+                    15.verticalSpace,
+                    DesktopHomeContainer(
+                      image: Image(
+                        width: 140.r,
+                        height: 140.r,
+                        fit: BoxFit.cover,
+                        image: const AssetImage(
+                          Assets.card3home,
+                        ),
+                      ),
+                      text: 'ايضا يمكنك رؤية الممرضين الذين هم في مهمات',
+                      color: AppColors.current.purpleButtons,
+                      backcolor: AppColors.current.darkPurple.withOpacity(0.5),
+                      test: true,
+                    )
+                  ],
+                ),
               ],
             ),
-            15.horizontalSpace,
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DesktopHomeContainer(
-                  image: Image(
-                    width: 140.r,
-                    height: 140.r,
-                    fit: BoxFit.cover,
-                    image: const AssetImage(
-                      Assets.card2home,
-                    ),
-                  ),
-                  text: 'رؤية الممرضين المتاحين',
-                  color: AppColors.current.orangeButtons,
-                  backcolor: AppColors.current.darkOrange.withOpacity(0.5),
-                  test: false,
-                ),
-                15.verticalSpace,
-                DesktopHomeContainer(
-                  image: Image(
-                    width: 140.r,
-                    height: 140.r,
-                    fit: BoxFit.cover,
-                    image: const AssetImage(
-                      Assets.card3home,
-                    ),
-                  ),
-                  text: 'رؤية الممرضين الذين هم في مهمات',
-                  color: AppColors.current.purpleButtons,
-                  backcolor: AppColors.current.darkPurple.withOpacity(0.5),
-                  test: true,
-                )
-              ],
-            ),
+            const Spacer(),
           ],
         ),
       ),
